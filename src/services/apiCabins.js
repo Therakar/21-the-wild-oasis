bfb10d69a916aa4ba530bdd27d582fd94089187a
Therakar <matteo.pau200@gmail.com>
2024-04-04 21:45:50 +0200

    Cabin Editing

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,21 +11,32 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+  console.log(newCabin, id);
+
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     ""
   );
 
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
   // https://nkujsraxertssgrmttjd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
 
-  // 1. Create cabin
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }])
-    .select();
+  // 1. Create/edit cabin IF there is no id
+  let query = supabase.from("cabins");
+
+  // a. CREATE CABIN (notice I'm placing { ...newCabin, image: imagePath } inside an array here)
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  // b. EDIT CABIN (notice I'm NOT placing { ...newCabin, image: imagePath } inside an array here)
+  if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+
+  const { data, error } = await query.select().single();
 
   if (error) {
     console.error(error);
